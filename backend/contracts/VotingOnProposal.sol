ec92d2f9055306136d16b3fcb35c52a809ff93bb
Aditya Gupta <adityagupta765469@gmail.com>
2022-07-30 23:27:32 +0530

    video link add

---
+++ backend/contracts/VotingOnProposal.sol
@@ -0,0 +1,13 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+contract VotingOnProposal{
+    struct Proposal{
+        address from;
+        string pdfLink;
+        string videolink;
+        uint8 numberOfResearchers;
+        uint256 numberOfResearches;
+        uint256 timestamp;
+    }
+}
