89f3c3d12853adb5f6f4acfaf439721e36f23920
Aditya Gupta <adityagupta765469@gmail.com>
2022-08-01 01:09:01 +0530

    smart contract updates

--- backend/contracts/VotingOnProposal.sol
+++ backend/contracts/VotingOnProposal.sol
@@ -2,12 +2,40 @@
 pragma solidity ^0.8.10;
 
 contract VotingOnProposal{
+
+    uint256 proposalCount = 0;
+
+    // details a proposal holds
     struct Proposal{
         address from;
         string pdfLink;
         string videolink;
-        uint8 numberOfResearchers;
-        uint256 numberOfResearches;
         uint256 timestamp;
     }
+
+    struct DAOMember{
+        bool ownsNFT;
+    }
+
+    // a vote can be yes or a no
+    enum Vote{
+        Yes, // 1
+        No // 0 
+    }
+
+    // this array stores all the proposals
+    Proposal[] public proposals;
+
+    // mapping to keep track of voters voting on proposals, where `address` is the address of the voter, `uint256` is the proposal index and `bool` is to keep track if the voter has voted on the proposal or not
+    mapping(address => mapping(uint256 => bool)) hasVoted;
+
+    modifier isADAOMember{
+        require(hasVoted[msg.sender][proposalCount] == true, "You need to be a member of DAO!");
+        _;
+    }
+
+
+    // function addProposal(string pdfLink, string videoLink) public isADAOMember(){
+    //     proposals.push(Proposal(msg.sender, pdfLink, videoLink, block.timestamp));
+    // }
 }
--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,6 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.9",
+   solidity: "0.8.10",
+  networks: {
+    mumbai: {
+      url: process.env.TESTNET_RPC,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+  }
 };
