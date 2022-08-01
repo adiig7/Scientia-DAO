13e26c39a3bd440076fae1c4cef96e20a071ae69
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2022-08-01 21:05:28 +0530

    added hero image and explore button

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -2,7 +2,9 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import hero from "../src/assets/hero.gif";
-import book from "../src/assets/book.gif";
+// import book from "../src/assets/book.gif";
+// import book from "../src/assets/hero.jpg";
+import book from "../src/assets/hero.png";
 import FeaturesCard from "../src/components/FeaturesCard";
 
 export default function Home() {
@@ -15,30 +17,36 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <div className={styles.title}>
-          <span className={`${styles.titleWord} ${styles.word1}`}>
-            Scientia{" "}
-          </span>
-          <span className={`${styles.titleWord} ${styles.word2}`}>DAO</span>
+        <div>
+          <div className={styles.title}>
+            <span className={`${styles.titleWord} ${styles.word1}`}>
+              Scientia{" "}
+            </span>
+            <span className={`${styles.titleWord} ${styles.word2}`}>DAO</span>
+          </div>
+          <div className={styles.tagline}>
+            <span className={`${styles.titleWord} ${styles.word2}`}>
+              Publish & share{" "}
+            </span>
+            <span className={`${styles.titleWord} ${styles.word1}`}>
+              your research on the
+            </span>
+            <span className={`${styles.titleWord} ${styles.word2}`}>
+              {" "}
+              world&#39;s first DAO{" "}
+            </span>
+            <span className={`${styles.titleWord} ${styles.word1}`}>
+              community for SCIENTISTS
+            </span>
+          </div>
+          <button className={styles.button} role="button">
+            Explore DAO
+          </button>
         </div>
+
         <div className={styles.hero}>
           {/* <Image src={hero} /> */}
           <Image src={book} />
-        </div>
-        <div className={styles.tagline}>
-          <span className={`${styles.titleWord} ${styles.word2}`}>
-            Publish & share{" "}
-          </span>
-          <span className={`${styles.titleWord} ${styles.word1}`}>
-            your research on the
-          </span>
-          <span className={`${styles.titleWord} ${styles.word2}`}>
-            {" "}
-            world&#39;s first DAO{" "}
-          </span>
-          <span className={`${styles.titleWord} ${styles.word1}`}>
-            community for SCIENTISTS
-          </span>
         </div>
       </main>
 
@@ -52,12 +60,9 @@ export default function Home() {
         </div>
 
         <div className={styles.wrapper}>
-
-        <FeaturesCard/>
-        <FeaturesCard/>
-      </div>
-      
-      
+          <FeaturesCard />
+          <FeaturesCard />
+        </div>
       </div>
     </div>
   );
