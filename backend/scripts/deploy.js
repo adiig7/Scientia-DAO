865496142b4b61374afdff7591d19f83d9cb31a7
Nils Giebing <11206675+nilsgiebing@users.noreply.github.com>
2022-08-01 11:46:26 +0200

    added deployment for contract VotingOnProposal
    Deploy contract VotingOnProposal on Polygon Mumbai Testnet.

--- backend/scripts/deploy.js
+++ backend/scripts/deploy.js
@@ -1,29 +1,29 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const { ethers } = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  /*
+  A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
+  so whitelistContract here is a factory for instances of our Whitelist contract.
+  */
+  const votingOnProposalContract = await ethers.getContractFactory("VotingOnProposal");
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  // deploy the contract
+  const deployedVotingOnProposalContract = await votingOnProposalContract.deploy();
+  
+  // wait for it to finish deploying
+  await deployedVotingOnProposalContract.deployed();
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log("Lock with 1 ETH deployed to:", lock.address);
+  // print the address of the deployed contract
+  console.log(
+    "VotingOnProposal Contract Address:",
+    deployedVotingOnProposalContract.address
+  );
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+// Call the main function and catch if there is any error
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
