3b98ce11dc1209007a37360fd69cef1932a4a371
Nils Giebing <11206675+nilsgiebing@users.noreply.github.com>
2022-08-01 11:50:55 +0200

    Update deploy.js

--- backend/scripts/deploy.js
+++ backend/scripts/deploy.js
@@ -1,10 +1,7 @@
 const { ethers } = require("hardhat");
 
 async function main() {
-  /*
-  A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
-  so whitelistContract here is a factory for instances of our Whitelist contract.
-  */
+  
   const votingOnProposalContract = await ethers.getContractFactory("VotingOnProposal");
 
   // deploy the contract
