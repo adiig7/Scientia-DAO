1e8f76c08546beb20328bf748f53c9d2c15d1c0d
dhruv <dhruvagarwal2017@gmail.com>
2022-08-02 23:38:36 +0530

    Contract update

--- backend/contracts/Whitelist.sol
+++ backend/contracts/Whitelist.sol
@@ -1,9 +1,7 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.10;
 
-
 contract Whitelist {
-
     // Max number of whitelisted addresses allowed
     uint8 public maxWhitelistedAddresses;
 
@@ -17,7 +15,7 @@ contract Whitelist {
     // Setting the Max number of whitelisted addresses
     // declare the Max number in index.js file
     constructor(uint8 _maxWhitelistedAddresses) {
-        maxWhitelistedAddresses =  _maxWhitelistedAddresses;
+        maxWhitelistedAddresses = _maxWhitelistedAddresses;
     }
 
     /*
@@ -26,13 +24,18 @@ contract Whitelist {
      */
     function addAddressToWhitelist() public {
         // check if the user has already been whitelisted
-        require(!whitelistedAddresses[msg.sender], "Sender has already been whitelisted");
+        require(
+            !whitelistedAddresses[msg.sender],
+            "Sender has already been whitelisted"
+        );
         // check if the numAddressesWhitelisted < maxWhitelistedAddresses, if not then throw an error.
-        require(numAddressesWhitelisted < maxWhitelistedAddresses, "More addresses cant be added, limit reached");
+        require(
+            numAddressesWhitelisted < maxWhitelistedAddresses,
+            "More addresses cant be added, limit reached"
+        );
         // Add the address which called the function to the whitelistedAddress array
         whitelistedAddresses[msg.sender] = true;
         // Increase the number of whitelisted addresses
         numAddressesWhitelisted += 1;
     }
-
 }
