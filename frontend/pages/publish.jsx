beced0d0d78d122044b1d44091f662bf5085019c
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2022-08-08 12:07:57 +0530

    publish page

--- frontend/pages/publish.jsx
+++ frontend/pages/publish.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../styles/Publish.module.css";
 
 export default function () {
+  // const [file, setFile] = useState([]);
+
   return (
     <>
       <main className={styles.main}>
@@ -9,13 +11,25 @@ export default function () {
           <span className={`${styles.titleWord} ${styles.word2}`}>
             Publish{" "}
           </span>
-          <span className={`${styles.titleWord} ${styles.word1}`}>Research</span>
+          <span className={`${styles.titleWord} ${styles.word1}`}>
+            Research
+          </span>
         </div>
 
         <div className={styles.publish}>
-          <p>Kushagra Kushagra Kushagra Kushagra Kushagra</p>
-
-          <p>Kushagra Kushagra Kushagra Kushagra Kushagra</p>
+          Enter Research Title
+          <input className={styles.research_title} type="text" placeholder="Research Title Here" />
+          Enter Research Description <small className={styles.small}> &#40; Minimum 500 words &#41;</small>
+          <textarea className={styles.research_desc}
+            name=""
+            id=""
+            placeholder="Enter Research Details Here"
+          ></textarea>
+          Select Research Media Files
+          <input className={styles.research_docs} type="file" multiple/>
+          <button className={styles.button}> Upload Research to IPFS </button>
+         
+          
         </div>
       </main>
     </>
