d56399526e2f30a72393c34f391aed43796799b1
Aditya Gupta <adityagupta765469@gmail.com>
2022-08-08 19:14:44 +0530

    new commit

--- backend/contracts/DAOMember.sol
+++ backend/contracts/DAOMember.sol
@@ -2,19 +2,27 @@
 
 pragma solidity ^0.8.10;
 
+interface 
+
 contract DAOMember{
     struct ResearchPaper{
         address researcher;
         string dateOfPublication;
         string researchPaperURI;
     }
-    struct Member{
+    struct Member {
         address memberAddress;
+        string name;
+        string bio;
+        string pfpURI;
+        string foR;
         string[] researchesURI;
     }
 
+
     uint256 counterResearches = 0;
     uint256 counterMembers = 0;
+    uint256 counterRequestList = 0;
 
     mapping(uint256 => Member) public membersList;  
     mapping(address => ResearchPaper) public membersPaperList;  
@@ -28,7 +36,7 @@ contract DAOMember{
     } 
 
 //TODO: need to complete this addMember function
-    function addMember(){
-        
+    function addMember(address _member) is OnlyDAOMember{
+        requestList[counterRequestList] = ResearchPaper(msg.sender,block.timestamp, researchPaperURI);
     }
 }
