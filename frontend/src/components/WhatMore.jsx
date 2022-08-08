e0f37f9bfdbb689eddda90eb0f9230acfddf3bdc
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2022-08-08 12:19:53 +0530

    added publish page

---
+++ frontend/src/components/WhatMore.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import styles from "../../styles/Home.module.css";
+
+export default function WhatMore() {
+  return (
+    <>
+      <div className={styles.what_more}>
+        <div className={styles.title}>
+          <span className={`${styles.titleWord} ${styles.word2}`}>What </span>
+          <span className={`${styles.titleWord} ${styles.word1}`}>More</span>
+          <span className={`${styles.titleWord} ${styles.word2}`}> to</span>
+          <span className={`${styles.titleWord} ${styles.word1}`}>
+            {" "}
+            Expect ?
+          </span>
+        </div>
+        <div className={styles.upcoming}>
+          <p>
+            We have planned to add more feature to <b>Scientia DAO</b> in future
+            and will be working on implementing those as we move forward with the project.
+            To give you all a glimpse of of what we are planning, here are the few things that we
+            will be working on to improve this project:
+          </p>
+
+            <li>🚀 Milestone feature for Grants</li> 
+            <li>🚀 Token and Governance Setup</li> 
+            <li>🚀 License and patent feature for published researches</li> 
+        </div>
+      </div>
+    </>
+  );
+}
