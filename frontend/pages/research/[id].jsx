99394c9624a486c18802a53ce7e8f6865f7592a1
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2022-08-08 15:22:44 +0530

    design changes in research card

---
+++ frontend/pages/research/[id].jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "../../styles/Research.module.css";
+
+export default function () {
+  return (
+    <div className={styles.main}>
+      <h2>Research Title Here</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+        laudantium fuga itaque saepe, quasi quas id, accusamus commodi quaerat
+        quis odit earum amet vero, expedita deserunt explicabo similique
+        mollitia provident!
+      </p>
+    </div>
+  );
+}
--- frontend/src/components/ResearchCard.jsx
+++ frontend/src/components/ResearchCard.jsx
@@ -6,6 +6,15 @@ export default function ResearchCard() {
     <div className={styles.main}>
       <div className={styles.researches}>
         <h3>Research Title Here</h3>
+        {/* <div className={styles.wrap}>
+          <div>
+            <h3>Research Title Here</h3>
+          </div>
+          <div className={styles.upvote}>
+            <button className={styles.arrow}></button>
+          </div>
+        </div> */}
+
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil itaque
           repudiandae dolorum esse unde? Beatae sit cupiditate eveniet Lorem
