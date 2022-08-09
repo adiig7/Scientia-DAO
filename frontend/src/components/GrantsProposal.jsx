834bf327c4ba902c2abde58858d7fd58171389f4
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2022-08-09 19:17:05 +0530

    grant proposal section added

--- frontend/src/components/GrantsProposal.jsx
+++ frontend/src/components/GrantsProposal.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import styles from "../../styles/Member.module.css";
 
 export default function GrantsProposal() {
   return (
-    <main>
-        <h2>Apply for Grants</h2>
+    <main className={styles.main}>
+        <div className={styles.idea}>
+          Enter Research Idea 
+          <input
+            className={styles.research_title}
+            type="text"
+            placeholder="Research Idea Here"
+          />
+          <label htmlFor="">Describe your Idea</label>
+          <small className={styles.small}> &#40; minimum 200 words &#41;</small>
+          <textarea
+            className={styles.research_desc}
+            name=""
+            id=""
+            placeholder="Describe the research idea "
+          ></textarea>
+          Upload any media files  
+          <input className={styles.research_docs} type="file" multiple />
+          <div className={styles.center}>
+            <button className={styles.button}>Submit Proposal </button>
+          </div>
+        </div>
     </main>
   )
 }
--- frontend/src/components/NewMember.jsx
+++ frontend/src/components/NewMember.jsx
@@ -5,7 +5,7 @@ export default function NewMember() {
   return (
     <>
       <div className={styles.newMember}>
-        <div className={styles.title}>
+        <div className={styles.title_small}>
           <span className={`${styles.titleWord} ${styles.word2}`}>Create</span>
           <span className={`${styles.titleWord} ${styles.word1}`}> DAO</span>
           <span className={`${styles.titleWord} ${styles.word2}`}> Entry</span>
@@ -14,13 +14,16 @@ export default function NewMember() {
             Proposal
           </span>
         </div>
+        {/* <label>Profile Picture</label>
+        <input className={styles.research_docs} type="file" accept="image/*, image/png, image/jpeg" /> */}
         <label>Enter Your Name</label>
-        <input placeholder="Name" className={styles.member_name} type="text" />
+        <input required placeholder="Name" className={styles.member_name} type="text" />
         <label>Describe Yourself <small> &#40; minimum 150 words &#41; </small></label>
-        <textarea placeholder="Bio" className={styles.member_bio} type="text" />
-        <label>Field of Research <small> &#40; optional &#41; </small> </label>
-        <input placeholder="Research Field" className={styles.member_name} type="text" />
-        
+        <textarea required placeholder="Bio" className={styles.member_bio} type="text" />
+        <label>Field of Research </label>
+        <input required placeholder="Research Field" className={styles.member_name} type="text" />
+        <label>Previous Researches <small> &#40; optional &#41; </small> </label>
+        <input className={styles.member_name}/> 
         <div className={styles.center}>
           <button className={styles.button}>Submit Proposal</button>
         </div>
