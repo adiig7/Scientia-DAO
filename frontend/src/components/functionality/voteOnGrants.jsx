b192a60be11671bb95d88d8a59ff779d43d20d62
dhruv <dhruvagarwal2017@gmail.com>
2022-08-11 19:00:10 +0530

    Vote on Grants completed

--- frontend/src/components/functionality/voteOnGrants.jsx
+++ frontend/src/components/functionality/voteOnGrants.jsx
@@ -1,1 +1,40 @@
 /// to vote on the proposals of the Grants by the members
+import React, { useEffect, useState } from "react";
+import { useAccount, useContract, useProvider, useSigner } from "wagmi";
+import {
+  Grants_ABI,
+  Grants_Contract_Address,
+} from "../../../constants/constants";
+
+const VoteEntry = async () => {
+  const provider = useProvider();
+  const { data: signer } = useSigner();
+  const { address, isConnected } = useAccount();
+
+  const Grant_contract = useContract({
+    addressOrName: Grants_Contract_Address,
+    contractInterface: Grants_ABI,
+    signerOrProvider: signer || provider,
+  });
+
+  /// getting all the members of the DAO , Scientists who have already joined the DAO
+  const Vote = async (_vote, _id) => {
+    try {
+      console.log("Adding the Vote to the contract");
+      let vote = _vote === "YES" ? 0 : 1;
+      const tx = await Grant_contract.vote(vote, _id);
+      await tx.wait();
+      console.log("Voting Completed for the user");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const fetch = async () => {};
+
+  useEffect(() => {
+    get();
+  });
+};
+
+export default VoteEntry;
