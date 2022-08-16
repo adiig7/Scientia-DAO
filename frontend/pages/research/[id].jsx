9037559986462dff4793a5fb1ae6de4b419b9512
dhruv <dhruvagarwal2017@gmail.com>
2022-08-17 01:35:58 +0530

    Research Page added

--- frontend/pages/research/[id].jsx
+++ frontend/pages/research/[id].jsx
@@ -1,11 +1,79 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "../../styles/Research.module.css";
 import Head from "next/head";
 import Image from "next/image";
 import Contribute from "../../src/components/Contribute";
+import { useRouter } from "next/dist/client/router";
 // import { Contribute } from "../index";
+import { useAccount, useContract, useProvider, useSigner } from "wagmi";
+import {
+  DAOMember_ABI,
+  DAOMember_Contract_Address,
+} from "../../constants/constants";
 
 export default function () {
+  const [research, setResearch] = useState({});
+  const router = useRouter();
+  const { id } = router.query;
+
+  const provider = useProvider();
+  const { data: signer } = useSigner();
+  const { address, isConnected } = useAccount();
+
+  const Member_contract = useContract({
+    addressOrName: DAOMember_Contract_Address,
+    contractInterface: DAOMember_ABI,
+    signerOrProvider: signer || provider,
+  });
+
+  // fetched the member.json
+  const fetchIPFS = async (metadataURI) => {
+    try {
+      console.log("Fetching from IPFS ...");
+      const URL = `https://ipfs.io/ipfs/${metadataURI}/research.json`;
+      const response = await fetch(URL);
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // fetch the data from the contract and then fetched the same from IPFS
+  // returns an object that is then stored in the Array of request
+  const fetchRequests = async (_id) => {
+    try {
+      console.log(_id);
+      const request = await Member_contract.getResearch(_id);
+      console.log(request);
+      console.log(request.researchPaperURI);
+      const _date = new Date(
+        parseInt(request.dateOfPublication._hex)
+      ).toLocaleString();
+      console.log(_date);
+      const response = await fetchIPFS(request.researchPaperURI);
+      console.log(response);
+      const parsedRequest = {
+        Id: _id,
+        Title: response.Name,
+        Description: response.Description,
+        Content: response.Content,
+        Publisher: request.researcher,
+        DoP: _date,
+      };
+      console.log(parsedRequest);
+      setResearch(parsedRequest);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (id) {
+      fetchRequests(id);
+    }
+  }, [id]);
+
   let url =
     "https://ipfs.io/ipfs/bafybeiebtlvqo4tgm4gfgwnw4v7hlsoz7zf3coyvbt7j2d7ucabmiqyj7q/players/1.png";
 
@@ -21,86 +89,31 @@ export default function () {
       </Head>
 
       <main className={styles.main}>
-        <h2>Fritz Haber&#39;s Experiments in Life and Death</h2>
+        <h2>{research.Title}</h2>
         {/* <small>
           {" "}
           &#40; by 0x8c0Bf9B1BFa0dc00f7eC63ac528010Fbeb818CA6 &#41;
         </small> */}
         <div className={styles.research}>
-          <p>
-            In April of 1915, Allied forces were battling the German Army for
-            control of Ypres, a Flemish town in western Belgium. Months before,
-            fighting with many young and untested soldiers, the Germans had
-            taken heavy casualties there in a battle they called the Massacre of
-            the Innocents of Ypres. This time, they were determined to launch
-            their first major attack on the Western Front. With thousands of
-            French, British, Belgian and Canadian forces dug in around the town,
-            the Germans turned to Fritz Haber. In 1918, Haber would be awarded
-            the Nobel Prize in chemistry for his work in developing a method of
-            synthesizing ammonia from nitrogen in the air—the process that
-            enabled the production of fertilizer in quantities that
-            revolutionized agriculture worldwide. But in the winter of 1915,
-            Haber&#39;s thoughts turned to annihilating the Allies. For his
-            efforts directing a team of scientists on the front lines in World
-            War I, he would become known as the father of chemical warfare.
-            <br />
-            <br />
-            Fritz Haber was born in Breslau, Prussia (now Wroclaw, Poland), in
-            1868, and educated at the St. Elizabeth Classical School, where he
-            took an early interest in chemistry. After studying at the
-            University of Berlin, he transferred to the University of Heidelberg
-            in 1886 and studied under the famed German chemist Robert Bunsen.
-            Haber was ultimately appointed professor of physical chemistry and
-            electrochemistry at the Karlshruhe Institute of Technology. When
-            scientists warned that the world would not be able to produce enough
-            food to feed its growing human population in the 20th century, he
-            listened. Scientists knew nitrogen was crucial to plant life; they
-            also knew the earth&#39;s supply of usable quantities was quite
-            limited. But Haber discovered a way to convert the nitrogen gas in
-            the earth&#39;s atmosphere into a compound that could be used in
-            fertilizer. According to Vaclav Smil, a global agricultural
-            historian at the University of Manitoba in Winnipeg, the Haber–Bosch
-            process of synthesizing and manufacturing ammonia from nitrogen and
-            hydrogen (and later industrialized by Carl Bosch, Haber&#39;s
-            brother-in-law) was likely the most important technological
-            innovation of the 20th century. It sustains the food base for the
-            equivalent of half the world&#39;s population today.
-            <br />
-            <br />
-            In 1901, Haber married the brilliant chemist Clara Immerwahr, the
-            first woman to receive a doctorate from Breslau University. Years
-            before, she&#39;d spurned a marriage proposal from him to focus on
-            her studies and career. Like Haber, she converted from Judaism to
-            Christianity, and the couple settled in Karlsruhe. But it wasn&#39;t
-            long before Clara Haber&#39;s research took a back seat to the
-            demands of being a homemaker and, after the birth of their son in
-            1902, a mother. To keep her mind stimulated, she began collaborating
-            with her husband on a textbook on the thermodynamics of gas, and
-            tried to continue her own research, writing and speaking. As her
-            husband&#39;s reputation spread, she was incensed to learn that her
-            audiences assumed that he had written her lectures. Meanwhile,
-            Haber&#39;s career flourished, and around the start of World War I,
-            the German Army requested his help in the development of replacing
-            explosives in shells with poison gasses.
-          </p>
+          <p>{research.Description}</p>
           <h3>Research Media </h3>
           <div className={styles.media}>
             <div>
               <Image
-                src={url}
-                alt="Picture of the author"
+                src={research.Content}
+                alt="Media Files for the Research"
                 width={200}
                 height={200}
               />
             </div>
-            <div>
+            {/* <div>
               <Image
                 src={url}
                 alt="Picture of the author"
                 width={200}
                 height={200}
               />
-            </div>
+            </div> */}
           </div>
           <Contribute />
         </div>
