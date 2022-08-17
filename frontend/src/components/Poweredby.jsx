9207a4c4b06ca5e2b7c9ceb3c516cd065052ce59
nilsgiebing <nils.giebing@gmail.com>
2022-08-17 22:13:25 +0200

    added learnweb3 and removed chainlink and added links

--- frontend/src/components/Poweredby.jsx
+++ frontend/src/components/Poweredby.jsx
@@ -4,7 +4,7 @@ import web3storage from "../assets/web3storage.svg";
 import polygon from "../assets/polygon.svg";
 import ipfs from "../assets/ipfs.png";
 import spheron from "../assets/spheron.svg";
-import valist from "../assets/chainlink.svg";
+import learnweb3 from "../assets/learnweb3.png";
 import Image from 'next/image';
 
 export default function Poweredby() {
@@ -17,20 +17,30 @@ export default function Poweredby() {
         </div>
         <div className={styles.sponsor_images}>
           <div className={styles.sponsor}>
-            <Image src={polygon} />
-          </div>
-          <div className={`${styles.sponsor} ${styles.ipfs}`}>
-            <Image src={ipfs} />
+            <a href="https://learnweb3.io" target="_blank" rel="noreferrer">
+            <Image src={learnweb3} />
+            </a>
           </div>
           <div className={styles.sponsor}>
+            <a href="https://polygon.technology" target="_blank" rel="noreferrer">
+            <Image src={polygon} />
+            </a>
+          </div>
+          <div className={`${styles.sponsor} ${styles.ipfs}`}>
+            <a href="https://ipfs.io" target="_blank" rel="noreferrer">
+            <Image src={ipfs} />
+            </a>
+          </div>
+          <div className={styles.sponsor}>
+            <a href="https://web3.storage" target="_blank" rel="noreferrer">
             <Image src={web3storage} />
+            </a>
              <p>Web3.Storage</p>
           </div>
           <div className={styles.sponsor}>
+            <a href="https://spheron.network" target="_blank" rel="noreferrer">
             <Image src={spheron } />
-          </div>
-          <div className={`${styles.sponsor} ${styles.chainlink}`}>
-            <Image src={valist } />
+            </a>
           </div>
         </div>
       </div>
