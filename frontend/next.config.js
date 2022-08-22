13bf33d267d5fdc5a209b59c560aa038162af186
Nils Giebing <nils.giebing@gmail.com>
2022-08-22 15:14:52 +0200

    image optimization possible fix

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -10,6 +10,8 @@ module.exports = {
   images: {
     // here you can add the url's that you are planning 
    // to use inside your next/image.
+    loader: 'akamai',
+    path:'',
     domains: ["ipfs.io"],
   },
 
